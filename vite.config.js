4e585aef578c508df1c7be6bb416a089cca093b7
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-09-24 13:39:53 +0700

    enhance: dist and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://github.com/f-bahesna/personal-site.git/', // Replace with your repository name
   plugins: [vue()]
 })
