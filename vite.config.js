a6f7dd336a1e3b60678e0aa1573c305990c41ed9
Lianum Frada Bahesna <fbahezna@gmail.com>
2025-09-24 14:06:41 +0700

    chore: update dist and vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/f-bahesna/personal-site.git/', // Replace with your repository name
+  base: '/', 
   plugins: [vue()]
 })
